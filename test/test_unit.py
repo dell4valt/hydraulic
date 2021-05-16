a6c56cb57ce84a48362e7f09109e255de95d4b4e
Vladimir K <dell4valt@gmail.com>
2021-05-16 13:45:20 +0300

    Рефакторинг юнит тестов

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,19 +1,21 @@
 import sys
-sys.path.insert(0, r'D:\github\hydraulic')
+from pathlib import Path
+p = Path(__file__).parents[1].absolute()
 
-import pytest
-from hydraulic import profile, lib
+sys.path.append(str(p.absolute()))
+from hydraulic import lib
+
 
 # Квадрат
 def test_poly_area_1():
     assert lib.poly_area([0, 0, 1, 1], [0, 1, 1, 0]) == 1
 
+
 # Прямоугольник
 def test_poly_area_2():
     assert lib.poly_area([0, 0, 10, 10], [0, 5, 5, 0]) == 50
 
+
 # Трапеция
 def test_poly_area_3():
     assert lib.poly_area([0, 2.5, 7.5, 10], [0, 5, 5, 0]) == 37.5
-
-
